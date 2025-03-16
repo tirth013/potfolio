ae94e2b483c975c01219dd7a449a243e4c792302
tirth013 <tirthasodariya001@gmail.com>
2025-03-16 15:55:42 +0530

    Your commit message here

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -22,7 +22,7 @@ const Contact = () => {
     setFormStatus("submitting");
 
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
+      const response = await fetch("https://tirthbackend.onrender.com/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
